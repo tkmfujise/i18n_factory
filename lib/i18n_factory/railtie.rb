2316a2dd11311620c008f1a9e0cbf49102a76aed
FUJISE Takumi <106900036+tkmfujise@users.noreply.github.com>
2024-06-03 08:04:40 +0900

    move directory lib/generators/i18_factory to lib/generators

--- lib/i18n_factory/railtie.rb
+++ lib/i18n_factory/railtie.rb
@@ -4,7 +4,7 @@ module ActiveModel
   class Railtie < Rails::Railtie
     generators do |app|
       Rails::Generators.configure! app.config.generators
-      require_relative '../generators/i18n_factory/model_generator'
+      require_relative '../generators/model_generator'
     end
   end
 end
