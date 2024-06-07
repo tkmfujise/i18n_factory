77ca7ffaee2f81e0dd70f828df00f7203933bc47
FUJISE Takumi <106900036+tkmfujise@users.noreply.github.com>
2024-06-08 03:10:16 +0900

    rails g model で名前空間付きの場合のテストを追加

--- spec/generators/model_generator_spec.rb
+++ spec/generators/model_generator_spec.rb
@@ -4,8 +4,9 @@ require 'generators/model_generator'
 # RSpec.describe I18nFactory::Generators::ModelGenerator, type: :generator do
 RSpec.describe Rails::Generators::ModelGenerator, type: :generator do
   destination File.expand_path('../../tmp', __FILE__)
-  subject { run_generator %w(Post title content:text) }
+  subject { run_generator arguments }
   before { prepare_destination }
+  let(:arguments) { %w(Post title content:text) }
 
   let(:locale_dir) { Pathname(destination_root).join('config/locales') }
 
@@ -45,6 +46,34 @@ RSpec.describe Rails::Generators::ModelGenerator, type: :generator do
                 title: Title
                 content: Content
       YAML
+    end
+  end
+
+  context 'if namespaced model' do
+    shared_examples 'works' do
+      it 'create en.yml' do
+        expect{ subject }.not_to raise_error
+        expect(locale_dir.join('foo/bar').children.count).to eq 1
+        assert_file 'config/locales/foo/bar/en.yml', <<~YAML
+          en:
+            activerecord:
+              models:
+                foo/bar: Foo::Bar
+              attributes:
+                foo/bar:
+                  name: Name
+        YAML
+      end
+    end
+
+    context 'slash' do
+      let(:arguments) { %w(foo/bar name) }
+      include_examples 'works'
+    end
+    
+    context 'colon' do
+      let(:arguments) { %w(Foo::Bar name) }
+      include_examples 'works'
     end
   end
 end
